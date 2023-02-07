581d597f0fa2479ae4ce746a086545456eb6173c
Youssef Youssef <youssefyoussef28@gmail.com>
2023-02-07 13:57:07 +0200

    Link and replace in NextJS

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href={"/blog"}>Blog</Link>
+      <Link href={"/product"}>Products</Link>
+    </div>
+  );
 }
 
 export default Home;
--- next-routing/pages/product/[productId]/index.js
+++ next-routing/pages/product/[productId]/index.js
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 function ProductDetail() {
   const router = useRouter();
   const productId = router.query.productId;
+
+
+  
   return <h1>Details about product {productId}</h1>;
 }
 
