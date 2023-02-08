2c814336bec2257365eff3993aba1005f939677e
Youssef Youssef <youssefyoussef28@gmail.com>
2023-02-08 11:55:56 +0200

    Css-modules

---
+++ ninjalist/pages/404.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="not-found">
+        <h1>Oooops...</h1>
+        <h2>That page cannot be found.</h2>
+        <p>
+          Go back to the{" "}
+          <Link className="link" href={"/"}>
+            Homepage
+          </Link>{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -11,20 +11,22 @@ export default function Home() {
     <>
       {/* <Navbar /> */}
       <br></br>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cum
         itaque consequatur dolorem blanditiis, magnam sit maxime repudiandae non
         omnis! Illum fuga ullam unde vel saepe modi distinctio in quod?
       </p>
       <br></br>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cum
         itaque consequatur dolorem blanditiis, magnam sit maxime repudiandae non
         omnis! Illum fuga ullam unde vel saepe modi distinctio in quod?
       </p>
       <br></br>
-      <Link href={"/ninjas"}>See Ninja Listing</Link>
+      <Link href={"/ninjas"} className={styles.btn}>
+        See Ninja Listing
+      </Link>
       {/* <Footer /> */}
     </>
   );
