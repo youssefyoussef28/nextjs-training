792d664a6c15c8c54c8044729d7c5ab9ae456195
Youssef Youssef <youssefyoussef28@gmail.com>
2023-02-10 16:57:47 +0200

    Fetching with GetStaticProps

--- ninjalist/pages/about.js
+++ ninjalist/pages/about.js
@@ -1,6 +1,12 @@
+import Head from "next/head";
+
 const About = () => {
   return (
     <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <h1>About</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cum
--- ninjalist/pages/ninjas/index.js
+++ ninjalist/pages/ninjas/index.js
@@ -1,7 +1,27 @@
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => {
+        return (
+          <div key={ninja.id}>
+            <a>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };
